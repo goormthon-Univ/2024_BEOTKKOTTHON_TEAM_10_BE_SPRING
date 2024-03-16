e12124ad947b258708b3a59ddb4582f68480d242
daeun-han <111632494+daeun-han@users.noreply.github.com>
2024-03-16 11:52:29 +0900

    feat: build project

---
+++ src/main/java/com/team10/cen/CenApplication.java
@@ -0,0 +1,13 @@
+package com.team10.cen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CenApplication.class, args);
+	}
+
+}
