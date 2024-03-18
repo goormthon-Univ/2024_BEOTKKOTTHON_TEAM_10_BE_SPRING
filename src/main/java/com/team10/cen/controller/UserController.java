3ee130f073a308dba1a538326d7d75d51f9e9e2e
daeun han <daeunhan0723@gmail.com>
2024-03-18 21:06:33 +0900

    test: testApi

--- src/main/java/com/team10/cen/controller/UserController.java
+++ src/main/java/com/team10/cen/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -32,5 +33,10 @@ public class UserController {
             return ResponseEntity.notFound().build();
         }
         return ResponseEntity.ok(user);
+    }
+
+    @GetMapping("/test")
+    public ResponseEntity<String> testEndpoint(@RequestHeader("userid") String userId) {
+        return ResponseEntity.ok("User ID: " + userId);
     }
 }
