9b1ea5e4eb835f39e2ff12b20071fbc5799c7e66
daeun han <daeunhan0723@gmail.com>
2024-03-20 13:47:16 +0900

    feat: d-day 계산

--- src/main/java/com/team10/cen/domain/Scholarship.java
+++ src/main/java/com/team10/cen/domain/Scholarship.java
@@ -3,6 +3,11 @@ package com.team10.cen.domain;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 @Entity
 @Getter @Setter
@@ -38,4 +43,31 @@ public class Scholarship {
     private String requiredDocuments;
 
     private String site;
+
+    private String supportTarget;
+
+    private Integer dDay;
+
+    // Callback method to calculate and set D-day before persisting
+    @PrePersist
+    public void calculateAndSetDday() {
+        if (endDate != null && !endDate.isEmpty()) {
+            // 현재 날짜 가져오기
+            LocalDate currentDate = LocalDate.now();
+            System.out.println("Current Date: " + currentDate);
+
+            // 마감일을 LocalDate로 변환
+            LocalDate endDateLocalDate = LocalDate.parse(endDate, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+            System.out.println("Parsed End Date: " + endDateLocalDate);
+
+            // 현재 날짜와 마감일 간의 차이 계산
+            int dday = (int) currentDate.until(endDateLocalDate).getDays();
+            System.out.println("Calculated D-day: " + dday);
+
+            // Scholarship 엔티티에 D-day 정보 설정
+            setDDay(dday);
+        } else {
+            System.out.println("End Date is null or empty");
+        }
+    }
 }
